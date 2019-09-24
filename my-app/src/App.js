b91a51fc387416e7d1d5b5a0bc6f50e8d9835fb4
Joshua Holtsclaw <Jildsmiles@yahoo.com>
2019-09-24 19:06:26 -0400

    added rewards component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 import SignupForm from "./components/SignupForm";
-import loginForm from "./components/LoginForm"
-import Chores from "./components/Chores"
-import Family from "./components/Family"
-import NavBar from "./components/NavBar"
+import loginForm from "./components/LoginForm";
+import Chores from "./components/Chores";
+import Family from "./components/Family";
+import NavBar from "./components/NavBar";
+import Rewards from "./components/Rewards";
 import { Route } from 'react-router-dom';
 
 
@@ -14,9 +15,9 @@ function App() {
       <Route   path="/" component={NavBar} />
       <Route  exact path="/signup" component={SignupForm} />
       <Route  exact path="/login" component={loginForm} />
-      
       <Route  exact path="/" component={Family}/>
       <Route  exact path="/chores" component={Chores}/>
+      <Route exact path = "/rewards" component={Rewards} />
       
     </div>
   );
--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -7,7 +7,7 @@ import styled from "styled-components";
         display:flex;
         align-items:center;
         justify-content:space-around;
-        text-decoration: none;
+        
     `;
 const NavBar = () => {
     return (
@@ -15,6 +15,7 @@ const NavBar = () => {
             <h1>Chore Tracker!</h1>
             <Link className="nav-links" to="/">Family</Link>
             <Link className="nav-links" to="/Chores">Chores</Link>
+            <Link className="nav-links" to="/rewards">Rewards</Link>
             <Link className="nav-links" to="/settings">Settings</Link>
         </NavDiv>
     )
